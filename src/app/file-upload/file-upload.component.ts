3ce34f4129fe27786d2d9e612bc3467be3118c1d
Dakad <bpottered+github@gmail.com>
2018-06-25 23:45:10 +0200

    Complete FileUpload HTML

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 import {
   AngularFireStorage,
-  AngularFireUploadTask
+  AngularFireUploadTask,
+  AngularFireStorageReference
 } from 'angularfire2/storage';
 import { Observable } from 'rxjs';
-import { DocumentSnapshot } from 'angularfire2/firestore';
+import { tap, finalize } from 'rxjs/operators';
+import { DocumentSnapshot, AngularFirestore } from 'angularfire2/firestore';
 import { UploadTaskSnapshot } from 'angularfire2/storage/interfaces';
 
 @Component({
@@ -14,41 +16,62 @@ import { UploadTaskSnapshot } from 'angularfire2/storage/interfaces';
   styleUrls: ['./file-upload.component.css']
 })
 export class FileUploadComponent implements OnInit {
-  private task: AngularFireUploadTask;
+  private task: AngularFireUploadTask; // Main task
 
-  private pct: Observable<number>;
+  // Progress monitoring
+  private percentage: Observable<number>;
 
   private snapshot: Observable<any>;
 
+  // Download URL
   private downloadUrl: Observable<string>;
 
+  // State for dropzone CSS toggling
   private isHovered: boolean;
 
-  constructor(private storage: AngularFireStorage) {}
+  constructor(
+    private storage: AngularFireStorage,
+    private db: AngularFirestore
+  ) {}
 
   ngOnInit() {}
 
   startUpload(event: FileList): void {
     const file = event.item(0);
-    const fileType = file.type.split('/')[0];
 
-    if (fileType !== 'image') {
-      console.error(`Unsupported file type (${fileType}) :-( :`);
+    if (file.type.split('/')[0] !== 'image') {
+      console.error(`Unsupported file type (${file.type}) :-( :`);
       return;
     }
 
-    const path = `test/${new Date().getTime()}_${file.name}`;
+    const filePath:string = `pics/${new Date().getTime()}_${file.name}`;
 
     const customMetadata = { app: 'MY AngularFire-powered PWA' };
 
-    this.task = this.storage.upload(path, file, { customMetadata });
-    this.snapshot = this.task.snapshotChanges();
-    this.pct = this.task.percentageChanges();
+
+    // The main task
+    const fileRef = this.storage.ref(filePath);
+    this.task = this.storage.upload(filePath, file, { customMetadata });
+
+    // Progress monitoring
+    this.percentage = this.task.percentageChanges();
+    this.snapshot = this.task.snapshotChanges().pipe(
+      tap(snap => {
+        if (snap.bytesTransferred === snap.totalBytes) {
+          // Update firestore on completion
+          this.db.collection('pics').add({ path : filePath, size: snap.totalBytes });
+        }
+      }),
+      // get notified when the download URL is available
+      finalize(() => this.downloadUrl = fileRef.getDownloadURL() )
+ )
+    );
 
     // The file's download URL
     // this.downloadUrl = this.task.downloadURL();
   }
 
+  // Determines if the upload task is active
   isActive(snapshot: UploadTaskSnapshot): boolean {
     return (
       snapshot.state === 'running' &&
